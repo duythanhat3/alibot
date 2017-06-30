b7a049752664a91e9c8bf733a96a6f3b5e02e26e
VuDuyThanh <thanhjv87@gmail.com>
2017-06-30 23:45:04 +0700

    test

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -26,8 +26,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middlewareGroups = [
-        /*
-         * 'web' => [
+         'web' => [
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
@@ -36,7 +35,6 @@ class Kernel extends HttpKernel
             //\App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
-        */
 
         'api' => [
             'throttle:60,1',
